f9ec6fc9825628ff82e3df7a9b6043e756a7a9b6
Kent Lee <kent.leee@gmail.com>
2019-03-16 17:36:22 -0700

    added multithreading functionality

--- deviantart-scraper.py
+++ deviantart-scraper.py
@@ -1,3 +1,5 @@
+from multiprocessing import Pool
+from functools import partial
 from html import unescape
 import requests
 import re
@@ -30,7 +32,8 @@ def get_author_name(html):
 
 
 # create directory and name it using author name
-def create_directory(author_name, download_location):
+def create_directory(author_name):
+    download_location = get_json("info.json")["download_location"]
     directory_path = download_location + "\\" + author_name
     if not os.path.exists(directory_path):
         os.makedirs(directory_path)
@@ -90,16 +93,15 @@ def get_thumb_urls(session, author_id, url):
 
 
 # download all images from a gallery url
-def download_images(session, author_id, download_location):
+def download_images(session, author_id):
     gallery_url = "https://www.deviantart.com/" + author_id + "/gallery/?catpath=/"
     gallery_html = get_unescape_html(session, gallery_url)
     if gallery_html is None:
         print("\nERROR: author id %s does not exist\n" % author_id)
         return
     author_name = get_author_name(gallery_html)
-    directory_path = create_directory(author_name, download_location)
+    directory_path = create_directory(author_name)
 
-    print("\ndownload for author %s begins\n" %  author_name)
     # process urls by chunks to improve execution time and memory consumption
     for image_urls in get_thumb_urls(session, author_id, gallery_url):
         for url in image_urls:
@@ -110,23 +112,23 @@ def download_images(session, author_id, download_location):
             file_name = get_file_name(response)
             file_path = directory_path + "\\" + file_name
             if os.path.isfile(file_path):
-                print("author %s is up-to-date\n" % author_name)
+                print("\nauthor %s is up-to-date\n" % author_name)
                 return
             print("download image: %s (%s)" % (image_title, file_name))
             with open(file_path, "wb") as f:
                 f.write(response.content)
-    print("\ndownload for author %s completed\n" % author_name)
 
 
 def main():
-    info = get_json("info.json")
+    author_ids = get_json("info.json")["author_ids"]
     # need session for the download button to work
     session = requests.Session()
-    # pass age restriction check
+    # bypass age restriction check
     session.cookies["agegate_state"] = "1"
-    print("there are %d authors..." % len(info["author_ids"]))
-    for id in info["author_ids"]:
-        download_images(session, id, info["download_location"])
+    print("\nthere are %d authors...\n" % len(author_ids))
+    # use all available cores, otherwise specify the number as an argument
+    with Pool() as pool:
+        pool.map(partial(download_images, session), author_ids)
 
 
 if __name__ == "__main__":
